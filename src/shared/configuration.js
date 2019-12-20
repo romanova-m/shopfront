e13ef8bb5dc9103cebac6fa388ffbd32f4ce7c95
Maria <123@gmail.com>
2019-12-20 20:17:53 +0300

    fixed config file

--- src/shared/configuration.js
+++ src/shared/configuration.js
@@ -1,6 +1,6 @@
 class Configuration {
     PET_COLLECTION_URL = "http://localhost/pet";
-    ITEM_COLLECTION_URL = "http://localhost:8086/stuff";
+    ITEM_COLLECTION_URL = "http://localhost/stuff";
     CART_COLLECTION_URL = "http://localhost/cart";
     BALANCE_COLLECTION_URL = "http://localhost/balance";
 }
